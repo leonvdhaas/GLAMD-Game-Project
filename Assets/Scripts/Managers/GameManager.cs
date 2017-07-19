3ec8468b6163f50884b9630399bdf6e3b927ef71
Anthony Timmers <anthonytimmers@hotmail.com>
2017-07-19 17:37:49 +0200

    Add Loader and multi tile variation support

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Managers
+{
+	public class GameManager
+		: MonoBehaviour
+	{
+		// Use this for initialization
+		private void Awake()
+		{
+			if (Instance == null)
+			{
+				Instance = this;
+
+			}
+			else if (Instance != this)
+			{
+				Destroy(gameObject);
+			}
+
+				DontDestroyOnLoad(gameObject);
+		}
+
+		public static GameManager Instance { get; private set; }
+
+		public static TileManager TileManager
+		{
+			get
+			{
+				return TileManager.Instance;
+			}
+		}
+	}
+}
