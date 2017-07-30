f350ad45bd12c2358667c5e613acd8cb99dae88f
ATimmeh33 <anthonytimmers@hotmail.com>
2017-07-30 15:31:25 +0200

    Add utility classes

---
+++ Assets/Scripts/Utility/Compressor.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace Assets.Scripts.Utility
+{
+	public static class Compressor
+	{
+		public static byte[] Zip(string input)
+		{
+			var bytes = Encoding.UTF8.GetBytes(input);
+
+			using (var msIn = new MemoryStream(bytes))
+			using (var msOut = new MemoryStream())
+			{
+				using (var gs = new GZipStream(msOut, CompressionMode.Compress))
+				{
+					CopyTo(msIn, gs);
+				}
+
+				return msOut.ToArray();
+			}
+		}
+
+		public static string Unzip(byte[] bytes)
+		{
+			using (var msIn = new MemoryStream(bytes))
+			using (var msOut = new MemoryStream())
+			{
+				using (var gs = new GZipStream(msIn, CompressionMode.Decompress))
+				{
+					CopyTo(gs, msOut);
+				}
+
+				return Encoding.UTF8.GetString(msOut.ToArray());
+			}
+		}
+
+		private static void CopyTo(Stream source, Stream destination)
+		{
+			byte[] bytes = new byte[4096];
+
+			int count;
+			while ((count = source.Read(bytes, 0, bytes.Length)) != 0)
+			{
+				destination.Write(bytes, 0, count);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Utility/Hasher.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Assets.Scripts.Utility
+{
+	public static class Hasher
+	{
+		public static string Encrypt(string input)
+		{
+			using (SHA512 algorithm = SHA512.Create())
+			{
+				var bytes = Encoding.UTF8.GetBytes(input);
+				var hash = algorithm.ComputeHash(bytes);
+				return Encoding.UTF8.GetString(hash);
+			}
+		}
+	}
+}
